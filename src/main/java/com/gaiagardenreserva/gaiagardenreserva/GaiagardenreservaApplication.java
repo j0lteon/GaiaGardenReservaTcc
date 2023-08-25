d21da092c417a76bd47e2ad2a1a2bd648ce1377e
Yasmin Feitosa do Nascimento <rm86087@estudante.fieb.edu.br>
2023-08-25 12:12:43 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/gaiagardenreserva/gaiagardenreserva/GaiagardenreservaApplication.java
@@ -0,0 +1,13 @@
+package com.gaiagardenreserva.gaiagardenreserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiagardenreservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaiagardenreservaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaiagardenreserva/gaiagardenreserva/GaiagardenreservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaiagardenreserva.gaiagardenreserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaiagardenreservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
